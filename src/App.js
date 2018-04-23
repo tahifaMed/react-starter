4eb89c21fde85260d6929eacc5bfb67f9c9f81d6
tahifa Med <tahifa.med@gmail.com>
2018-04-23 20:19:17 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Welcome Everyone to the React Lecture !!
+      </div>
+    );
+  }
+}
+
+export default App;
